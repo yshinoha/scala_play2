8d4944fd5699716714987963ea69ea573dc14520
SHINOHARAYUKI <yshinoha@SHINOHARAYUKI-no-MacBook-Air.local>
2014-05-05 14:10:41 +0900

    JSONファイル

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,22 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.libs.json._
+
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+
+   val JsValue = Json.parse("""
+  { 
+    "title": "チューリップ",
+    "image": "https://farm3.staticflickr.com/2901/14053822803_b2f446ea3b_h.jpg",
+    "dt": "2014/04/17"
+  }
+""") 
+
+
+    Ok(views.html.index("Your new application is ready.", JsValue))
   }
 
 }
